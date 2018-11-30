81f811aaec0649b1e4b712f5702ac0edb4666222
Tianyu Zhang <2437317275@qq.com>
2018-11-30 20:39:24 +0800

    Update currency.py

--- pyassign2/currency.py
+++ pyassign2/currency.py
@@ -1,3 +1,10 @@
+"""currency.py: To convert one currency into another at an exchange rate.
+
+__author__ = "Zhang Tianyu"
+__pkuid__  = "1800011759"
+__email__  = "1800011759@pku.edu.cn"
+"""
+
 def exchange(currency_from,currency_to,amount_from):
     """In this exchange, the user is changing amount_from money in 
     currency currency_from to the currency currency_to. The value 
@@ -7,7 +14,8 @@ def exchange(currency_from,currency_to,amount_from):
     Parameter currency_from is a string: the currency on hand
     Parameter currency_to is a string: the currency to convert to
     Parameter amount_from is a string: amount of currency to convert
-    Return is a float: amount of currency received in the given exchange."""
+    Return is a float: amount of currency received in the given exchange.
+    """
     import json
     from urllib.request import urlopen
     
@@ -29,12 +37,15 @@ def exchange(currency_from,currency_to,amount_from):
     
 def test_amount_to():
     """verify that the data values are correct and 
-    that the data types are correct (it should be a float)"""
+    that the data types are correct (it should be a float)
+    """
     assert(3.673162 == exchange('USD','AED',1)),'output error'
     assert(0.777993 == exchange('USD','IMP',1)),'output error'
+    assert(0.070368024769545 == exchange('UAH','USD',2)),'output error'
 
 def test_currency(): 
-    """verify that the user input currency type is correct"""
+    """verify that the user input currency type is correct
+    """
     assert('Source currency code is invalid.'==exchange('AAA','AED',1)),'validity verification error'
     
 def test_amount_from():
@@ -42,10 +53,17 @@ def test_amount_from():
     assert('Currency amount is invalid.'==exchange('USD','AED','a')),'validity verification error'
 
 def testAll():
-    """test all cases"""
+    """test all cases
+    """
     test_amount_to()
     test_currency()
     test_amount_from()
     print("All tests passed")
 
-testAll()
+def main():
+    """main module
+    """
+    testAll()
+    
+if __name__ == '__main__':
+    main()
